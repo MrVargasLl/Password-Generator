0d11af38556aff4411f3f879fba0d4b9beaff56d
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2025-11-12 12:44:28 -0400

    Trabajando en cuadro Generador Rev1

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const characterLength = document.querySelector("#range_length")
 // Variables de entrada
     const uppercase = document.querySelector("#uppercase")
     const lowercase = document.querySelector("#lowercase")
+    const numbers = document.querySelector("#numbers")
     const symbols = document.querySelector("#symbols")
 
 let genPassword = ""
@@ -16,21 +17,29 @@ botonGenerar.addEventListener("click", () => {
     const lowerTextletters = "abcdefghijklmnopqrstuvwxyz"
     const upperTextLetters = lowerTextletters.toUpperCase()
     const numberinc = "0123456789"
-    const symbols = "!$%&?@"
+    const sybols = "!$%&?@"
+    let totalValues = ""
 
+    if (uppercase.checked) {
+        totalValues = totalValues + upperTextLetters
+    }
+
+    if (lowercase.checked) {
+        totalValues = totalValues + lowerTextletters
+    }
+    
+    if (numbers.checked) {
+        totalValues = totalValues + numberinc
+    }
+    if (symbols.checked) {
+        totalValues = totalValues + sybols
+    }
     
     passwordText.textContent = genPassword
 
     for (let i=0; i< characterLength.value; i++) {
-
-    
-            
-        const randomIndex =  parseInt(Math.random()*Number(characterLength.value))
-
-        console.log(i, "---loop");
-        console.log(randomIndex, "----index in loop");
-
-        genPassword = genPassword + lowerTextletters [randomIndex]
+        const randomIndex =  parseInt(Math.random()*Number(totalValues.length))
+        genPassword = genPassword + totalValues [randomIndex]
     
     }
     
